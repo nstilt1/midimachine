fe6d8b48ee043ca98769efd7f9cd11116131439f
Noah Stiltner <nstilt1@lsu.edu>
2024-10-25 12:07:15 -0500

    adjusted styles

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -12,11 +12,11 @@ export default function Home() {
   return (
     <div>
       <Navbar whenClickedLogo={() => setClickedLogo(!clickedLogo)} />
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <MidiApp showExtraControls={clickedLogo}></MidiApp>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center p-8">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
