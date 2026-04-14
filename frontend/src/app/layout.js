923bb6b8081683796e849512ac03ecad219d2401
Noah Stiltner <nstilt1@lsu.edu>
2026-04-14 14:28:14 -0500

    fix suspense issue

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { Suspense } from "react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,7 +30,9 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Suspense>
+          {children}
+        </Suspense>
       </body>
     </html>
   );
