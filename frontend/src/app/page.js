47eb943612235dd5fc0e15a97f09fc94272254c1
Noah Stiltner <nstilt1@lsu.edu>
2026-04-14 13:56:24 -0500

    add embed mode

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -6,6 +6,7 @@ import { useEffect, useRef, useState } from "react";
 import useLocalStorage from "@/hooks/useLocalStorage";
 import Navbar from "@/components/NavbarMainSite";
 import ChordProgressionBuilder from "@/components/ChordProgressionBuilder";
+import { useSearchParams } from "next/navigation";
 
 export default function Home() {
   const [clickedLogo, setClickedLogo] = useLocalStorage("clickedLogo", false);
@@ -13,7 +14,8 @@ export default function Home() {
   const [buttonText, setButtonText] = useState(clickedLogo);
   const [wasmModule, setWasmModule] = useState(null);
   const [showBuilder, setShowBuilder] = useLocalStorage("showBuilder", false);
-
+  const searchParams = useSearchParams();
+  const isEmbed = searchParams.get("embed") === "1";
   useEffect(() => {
     setButtonText(clickedLogo ? "Hide advanced controls" : "Show advanced controls")
   }, [clickedLogo]);
@@ -34,7 +36,7 @@ export default function Home() {
 
   return (
     <div>
-      <Navbar whenClickedLogo={() => setClickedLogo(!clickedLogo)} />
+      {!isEmbed && <Navbar whenClickedLogo={() => setClickedLogo(!clickedLogo)} />}
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <MidiApp showExtraControls={clickedLogo} toggleExtraControls={() => {setClickedLogo(!clickedLogo)}} cpbRef={cpbRef} wasmModule={wasmModule}></MidiApp>
